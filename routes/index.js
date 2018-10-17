215b03b10c926ee8b39e932c96c15b9d1ff07298
Mahmoud Kamel <mahmoudkamel@outlook.com>
2018-10-17 14:18:09 +0200

    fix

--- routes/index.js
+++ routes/index.js
@@ -59,7 +59,6 @@ router.post('/getTN', type,function(req, res) {
 
 
 router.post('/findData', type,function(req, res) {
-      console.log(req.body)
       var findData = async (function (){
         if (req.body.type=='AI') 
           await (SearchCTRL.checkDataAI(req,res));
